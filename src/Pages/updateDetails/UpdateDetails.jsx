c00d30e47739606697c62ec661816fe2a3b77530
sayemp2 <sojibulislamsayem420@gmail.com>
2024-08-28 01:08:55 +0600

    update : updateSpot Deatils from frontend

--- src/Pages/updateDetails/UpdateDetails.jsx
+++ src/Pages/updateDetails/UpdateDetails.jsx
@@ -1,15 +1,42 @@
 import { useLoaderData } from "react-router-dom";
 
 const UpdateDetails = () => {
- const spotData = useLoaderData();
- const {image, spot_Name, country_Name, location, description, cost, season, time, visitors} = spotData;
+    const spotData = useLoaderData();
+    const { _id, image, spot_Name, country_Name, location, description, cost, season, time, visitors } = spotData;
+    const handleUpdate = e => {
+        e.preventDefault();
+        const form = e.target;
+        const image = form.image.value;
+        const spot_Name = form.touristSpot.value;
+        const country_Name = form.countryName.value;
+        const location = form.location.value;
+        const description = form.description.value;
+        const cost = form.cost.value;
+        const season = form.season.value;
+        const time = form.time.value;
+        const visitors = form.visitors.value;
+        const updateSpotDetails = { image, spot_Name, country_Name, location, description, cost, season, time, visitors }
+        fetch(`https://tourism-management-server-brown.vercel.app/spotlist/${_id}`,{
+            method:'PUT',
+            headers:{
+                'content-type':'application/json'
+            },
+            body: JSON.stringify(updateSpotDetails)
+        })
+        .then(res=> res.json())
+        .then(data=>{
+            console.log(data);
+        })
+        
 
-    
+    }
+
+
     return (
         <div>
             <div className="bg-[#e9f2ff] flex lg:flex-row flex-col lg:items-center">
                 <div className="lg:w-1/2 p-4 lg:pl-7 animate-slideInLeft">
-                    <form  className="space-y-3 bg-teal-500 p-11 rounded-2xl">
+                    <form onSubmit={handleUpdate} className="space-y-3 bg-teal-500 p-11 rounded-2xl">
                         <div>
                             <label className="font-Montserrat text-white text-xl font-semibold" htmlFor="image">Image Url</label><br />
                             <input className="input font-Montserrat text-xl w-full text-black bg-white" placeholder="Enter the photo url" defaultValue={image} type="text" name="image" id="image" required />
@@ -52,7 +79,7 @@ const UpdateDetails = () => {
                                 <input className="input font-Montserrat text-xl w-full text-black bg-white" placeholder="Type Total Visitors" defaultValue={visitors} type="text" name="visitors" id="visitors" required />
                             </div>
                         </div>
-                        <input className="btn font-Montserrat text-xl font-bold w-full " type="submit" value="Submit" />
+                        <input className="btn font-Montserrat text-xl font-bold w-full " type="submit" value="Update" />
                     </form>
                 </div>
                 <div className="lg:w-1/2 animate-slideInRight">
