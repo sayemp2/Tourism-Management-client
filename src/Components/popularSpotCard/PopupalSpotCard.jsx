ef5de1b5f8be3c25e17b1fa80c10d4420f26154d
sayemp2 <sojibulislamsayem420@gmail.com>
2024-08-30 15:49:51 +0600

    update ui

--- src/Components/popularSpotCard/PopupalSpotCard.jsx
+++ src/Components/popularSpotCard/PopupalSpotCard.jsx
@@ -1,17 +1,20 @@
+import { MdLocationPin } from "react-icons/md";
+
 const PopupalSpotCard = ({ touristSpot }) => {
-    const { image, spot_Name, cost, location, visitors, time} = touristSpot
+    const { image, spot_Name, cost, country_Name, location, visitors, time} = touristSpot
     return (
-        <div className="bg-[#2b4b1d30] p-4 md:w-96 shadow-xl mx-auto">
-            <div className="overflow-hidden ">
+        <div className="bg-[#1c311330] rounded-xl md:w-96 shadow-xl mx-auto">
+            <div className="overflow-hidden rounded-t-xl ">
                 <img className="duration-300 hover:scale-110" src={image} alt="" />
             </div>
-            <div>
-                <h1 className="text-4xl text-white font-semibold font-Montserrat">{spot_Name}</h1>
-                <p className="font-Montserrat font-medium"><span className="text-xl font-bold text-[#63ab45]">${cost}</span>/Per person</p>
-                <div className="flex">
-                    <p>{time}DAYS</p>
-                    <p>{visitors}+</p>
-                    <p>{location}</p>
+            <div className="p-5 px-8 pb-11 mt-5">
+                <p className="flex items-center font-Montserrat text-[14px] font-semibold"><MdLocationPin className="text-[#63ab45] text-xl"/>{location}</p>
+                <h1 className="text-4xl text-white font-semibold font-Montserrat mt-1">{spot_Name}</h1>
+                <p className="font-Montserrat font-medium mt-3"><span className="text-xl font-bold text-[#63ab45]">${cost}</span>/Per person</p>
+                <div className="flex space-x-2 mt-5">
+                    <p className="bg-[#ffffff41] px-3 font-Montserrat font-semibold duration-300 py-1 text-xs hover:text-[#63ab45] rounded-2xl">{time} DAYS</p>
+                    <p className="bg-[#ffffff41] px-3 font-Montserrat font-semibold duration-300 py-1 text-xs hover:text-[#63ab45] rounded-2xl">{visitors}+</p>
+                    <p className="bg-[#ffffff41] px-3 font-Montserrat font-semibold duration-300 py-1 text-xs hover:text-[#63ab45] rounded-2xl">{country_Name}</p>
                 </div>
             </div>
 
--- src/Components/spotSection/SpotSection.jsx
+++ src/Components/spotSection/SpotSection.jsx
@@ -5,10 +5,10 @@ const SpotSection = ({ spotlist }) => {
     return (
         <div>
              <div className="mx-auto text-center mb-5">
-                <h3 className="lg:text-2xl mb-6 font-bold text-[#63ab45] font-Montserrat">Popular Tours</h3>
-                <h1 className="md:text-5xl mb-4 text-2xl font-bold text-white font-Montserrat">Most Popular Places</h1>
+                <h3 className="lg:text-2xl mb-2 font-bold text-[#63ab45] font-Montserrat">Popular Tours</h3>
+                <h1 className="md:text-5xl mb-6 text-2xl font-bold text-white font-Montserrat">Most Popular Places</h1>
             </div>
-            <div className="grid gap-5 lg:grid-cols-3 md:grid-cols-2">
+            <div className="grid lg:grid-cols-3 md:mx-6 md:grid-cols-2">
                 {spotlist.slice(0, 3).map((touristSpot) => (
                     <PopupalSpotCard key={touristSpot._id} touristSpot={touristSpot}></PopupalSpotCard>
                 ))}
